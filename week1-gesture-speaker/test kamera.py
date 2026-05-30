bd9c40983395a3039f16ae26edc2c51dde0942c4
@sep enterprise <asepvicesta@gmail.com>
2026-05-30 14:03:38 +0700

    Day1 Ujicoba kamera dengan indikasi teks dan resolusi [SELESAI]

--- week1-gesture-speaker/test kamera.py
+++ week1-gesture-speaker/test kamera.py
@@ -14,10 +14,23 @@ while True:
         print("GAGAL MENGAMBIL FRAME")
         break
 
+    org = (10, 30)
+    fontFace = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 1
+    color = (0, 255, 0)
+    thickness = 2
+    cv2.putText(frame, "KAMERA AKTIF", org, fontFace, fontScale, color, thickness)
+
+    lebar = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
+    tinggi = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    teks = f"Resolusi: {int(lebar)}x{int(tinggi)}"
+    cv2.putText(frame, teks, (10, int(tinggi) - 20), fontFace, 0.7, (255, 255, 0), 2)
+
     cv2.imshow('test kamera', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
-        print("GAGAL MENGAKSES PROGRAM")
+        print("PROGRAM DITUTUP")
         break
 
 cap.release()
