1c4b841d349fc846a5140c2d83ab9a4133f74310
Asepenterprise <asepvicesta@gmail.com>
2026-06-06 23:28:16 +0700

    Update test_face.py

--- week2-face-absen/week2-face-absen/tests/test_face.py
+++ week2-face-absen/week2-face-absen/tests/test_face.py
@@ -1,5 +1,4 @@
-# test_face.py
-# Tujuan: Test deteksi wajah pertama kali
+
 
 import cv2
 import face_recognition
@@ -11,17 +10,14 @@ while True:
     ret, frame = cap.read()
     if not ret:
         continue
-
-    # Konversi BGR ke RGB
+        
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
     kecil = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     rgb = cv2.cvtColor(kecil, cv2.COLOR_BGR2RGB )
     lokasi_wajah = face_recognition.face_locations(rgb)
     lokasi_wajah = [(t*4, r*4, b*4, l*4) for t,r,b,l in lokasi_wajah]
-    
 
-    # Gambar kotak di sekitar wajah
     for top, right, bottom, left in lokasi_wajah:
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
         cv2.putText(frame, "Ashraf ganteng", (left, top-10),
